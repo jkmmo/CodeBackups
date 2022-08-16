7c9cbe83c57af6a764ae45f1dd882dbf6108ffc9
jkmmo <31679886+jkmmo@users.noreply.github.com>
2022-08-15 22:24:55 -0400

    v1.1
    returns (Index,Value,Old)
    --index = ValueName
    --Value = NewData
    --Old = value before change

--- observer.lua
+++ observer.lua
@@ -2,42 +2,42 @@ local Observer = {}
 local Https = game:GetService("HttpService")
 
 function Observer.new(MyTable,Callback)
-    local St = {}
-    local Mt = {}
-    local Constructing = true
-    local Var = false
-    setmetatable(St,{
-        __index = function(self,key)
-            return Mt[key]
-        end,
-            
-        __newindex = function(self,Key,Value)
-            if not Constructing then
-                Callback(Key,Value)
-            end
-            Mt[Key] = Value
-        end,
-        __tostring = function(self)
-            if Var == true then
-                return tostring(Mt.Value)
-            else
-                local s : string = Https:JSONEncode(Mt):gsub(",","\n"):gsub("{","\n{\n"):gsub("}","\n}")
-                return s
-            end
-        end
-    })
-    
-    if type(MyTable) == "table" then
-        for i,v in pairs(MyTable) do
-            St[i] = v
-        end
-    else
-        St.Value = MyTable
-        Var = true
-    end
-    
-    Constructing = false
-    return St
+	local St = {}
+	local Mt = {}
+	local Constructing = true
+	local Var = false
+	setmetatable(St,{
+		__index = function(self,key)
+			return Mt[key]
+		end,
+
+		__newindex = function(self,Key,Value)
+			if not Constructing then
+				Callback(Key,Value,Mt[Key])
+			end
+			Mt[Key] = Value
+		end,
+		__tostring = function(self)
+			if Var == true then
+				return tostring(Mt.Value)
+			else
+				local s : string = Https:JSONEncode(Mt):gsub(",","\n"):gsub("{","\n{\n"):gsub("}","\n}")
+				return s
+			end
+		end
+	})
+
+	if type(MyTable) == "table" then
+		for i,v in pairs(MyTable) do
+			St[i] = v
+		end
+	else
+		St.Value = MyTable
+		Var = true
+	end
+
+	Constructing = false
+	return St
 end
 
 return Observer
